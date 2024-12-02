b25880cbee0f6f4e56c9a73abed5368294266bc7
Serpantiner <85799080+Serpantiner@users.noreply.github.com>
2024-12-02 18:11:09 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,20 @@
 package main
 
 import (
-    "fmt"
-    "advent/day1"
+   "fmt"
+   "advent/day1"
+   "advent/day2" 
 )
 
 func main() {
-    path := "input.txt"
-    fmt.Println(day1.CalculateDistance(path))
-    fmt.Println(day1.CalculateSimilarityScore(path))
+   path1 := "input.txt"
+   path2 := "day2/input_day_2.txt"
+
+   fmt.Println("Day 1:")
+   fmt.Printf("Total distance: %d\n", day1.CalculateDistance(path1))
+   fmt.Printf("Similarity score: %d\n", day1.CalculateSimilarityScore(path1))
+
+   fmt.Println("\nDay 2:")
+   fmt.Printf("Number of safe reports: %d\n", day2.CheckSafeReports(path2))
+   fmt.Printf("Number of safe reports with dampener: %d\n", day2.CheckSafeWithDampener(path2))
 }
